bd2623a37821724a85cee39b23cde57948d1965e
fkiptooh <fkiptooh@gmail.com>
2023-05-09 15:17:56 +0300

    Registration route

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const UserModel = require('../../models/UserModel');
 
 const router = express.Router();
 
 module.exports = () => {
   router.get('/registration', (req, res) => res.render('users/registration', { success: req.query.success }));
+
+  router.post('/registration', async(req, res, next) => {
+    const { username, email, password } = req.body;
+    try {
+      const user = new UserModel({
+        username,
+        email,
+        password
+      })
+      const savedUser = await user.save();
+      if (savedUser) return res.redirect('/user/registration?success=true')
+    } catch (error) {
+      
+    }
+  })
 
   router.get('/account', (req, res) => res.render('users/account', { user: req.user }));
 
