776f7aa827de88a8924c75372e93950a72f0fbf1
fkiptooh <fkiptooh@gmail.com>
2023-05-10 13:19:01 +0300

    App optimization- by default node is single threaded

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const compression = require('compression');
 const mongoose = require('mongoose');
 const MongoStore = require('connect-mongo')(session);
 const auth = require('./lib/auth')
@@ -14,6 +15,7 @@ const AvatarService = require('./services/AvatarService');
 
 module.exports = (config) => {
   const app = express();
+  app.use(compression());
   const speakers = new SpeakerService(config.data.speakers);
   const feedback = new FeedbackService(config.data.feedback);
   const avatars = new AvatarService(config.data.avatars);
